15a9a2b3ebf2f63ba6b7fa5fd8177423767ea350
cnlund <91978441+cnlund@users.noreply.github.com>
2023-11-20 21:36:27 -0500

    Creacion de la zona de html

--- ochurus.go
+++ ochurus.go
@@ -1,3 +1,15 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	//Zona de html
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(rw, "Hola Mundo")
+	})
+	//Aqui mandamos nuestra pagina web al puerto local 3000
+	http.ListenAndServe(":3000", nil)
+}
