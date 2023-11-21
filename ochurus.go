3222efd917c2710a49fb1f32456c223336c19783
cnlund <91978441+cnlund@users.noreply.github.com>
2023-11-21 03:15:09 -0500

    avance sql

--- ochurus.go
+++ ochurus.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
+var db *sql.DB
+
 func main() {
-	//Zona de html
+	//Zona de BDD
+	var err error
+	//Hacemos la conexion
+	db, err = sql.Open("sqlserver", "server=CNLUNDPC;port=1433; database=Prueba")
+	if err != nil {
+		log.Fatal("Error al conectarse con la BDD: " + err.Error())
+	}
+	log.Printf("Se conecto!!!")
+	defer db.Close()
+	rows, err := db.Query("INSERT INTO Tabla_prueba (id, dato_prueba) VALUES (4, 'isra');")
+	if err != nil {
+		log.Fatal("Error al insertar: " + err.Error())
+	}
+	defer rows.Close()
+
+	//Zona de HTML
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(rw, "Hola Mundo")
 	})
 	//Aqui mandamos nuestra pagina web al puerto local 3000
-	http.ListenAndServe(":3000", nil)
+	//http.ListenAndServe(":3000", nil)
 }
