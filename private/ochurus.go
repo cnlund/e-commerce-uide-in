f62d5ca766db4a0ded17e4d459169da228f04492
cnlund <91978441+cnlund@users.noreply.github.com>
2023-11-25 09:21:21 -0500

    lista desplegable ciudades

--- private/ochurus.go
+++ private/ochurus.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
@@ -13,15 +14,31 @@ import (
 var db *sql.DB
 
 // Zona de los Structs-------------------------------------------------------------------
-type Filtro struct {
-	Ciudad string
+// Struct para el buscador
+type Ciudad struct {
+	Nombre string
 }
+
+// Struct del formato de formulario de informacion sobre los postulantes
 type Format_Post struct {
-	Nombre  string
-	Edad    int
-	Ciudad  string
-	Correo  string
-	Carrera string
+	Nombre   string
+	Edad     int
+	Ciudad   string
+	Correo   string
+	Egresado Carrera
+}
+
+// Struct de las diversas habilidades que pueden haber en cada carrera
+type Skills struct {
+	Habilidad    string
+	Conoce       bool
+	Nivel_con    int
+	Carrera_pert Carrera
+}
+
+// Struct de las carreras con sus datos relevantes
+type Carrera struct {
+	Nombrec string
 }
 
 // Zona de los Handlers------------------------------------------------------------------
@@ -41,6 +58,22 @@ func postulanteHandler(rw http.ResponseWriter, r *http.Request) {
 	template.Execute(rw, nil)
 }
 
+// Handler de almacenamiento de ciudades
+func ciudadesHandler(rw http.ResponseWriter, r *http.Request) {
+	var cid int
+	rows2, _ := db.Query("SELECT Ciudad_id FROM Ciudades")
+	for rows2.Next() {
+		rows2.Scan(&cid)
+	}
+	var cnombre string
+	rows, _ := db.Query("SELECT Ciudad_nombre FROM Ciudades")
+	for rows.Next() {
+		rows.Scan(&cnombre)
+		cid := Ciudad{cnombre}
+		fmt.Fprintln(rw, cid.Nombre)
+	}
+}
+
 // Handler de la pagina de CONTRATANTES
 func contratantesHandler(rw http.ResponseWriter, r *http.Request) {
 	template, _ := template.ParseFiles("./public/contratar.html")
@@ -49,10 +82,12 @@ func contratantesHandler(rw http.ResponseWriter, r *http.Request) {
 
 // -----------------------------------------------------------------------------------------
 func main() {
+	//Creamos el server mux
+	mux := http.NewServeMux()
 	//Zona de BDD---------------------------------------------------------------------------
 	var err error
 	//Hacemos la conexion
-	db, err = sql.Open("sqlserver", "server=CNLUNDPC;port=1433; database=Prueba")
+	db, err = sql.Open("sqlserver", "server=CNLUNDPC;port=1433; database=Ochurus_DB")
 	if err != nil {
 		log.Fatal("Error al conectarse con la BDD: " + err.Error())
 	}
@@ -60,9 +95,10 @@ func main() {
 	defer db.Close()
 	// Zona HTML----------------------------------------------------------------------------
 	//Creamos los handlerfuncs
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/postular", postulanteHandler)
-	http.HandleFunc("/contratar", contratantesHandler)
-	//Aqui mandamos nuestra pagina web al puerto local 3000
-	http.ListenAndServe(":443", nil)
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/postular", postulanteHandler)
+	mux.HandleFunc("/postular/lciudades", ciudadesHandler)
+	mux.HandleFunc("/contratar", contratantesHandler)
+	//Aqui mandamos nuestra pagina web al puerto local 443
+	http.ListenAndServe(":443", mux)
 }
