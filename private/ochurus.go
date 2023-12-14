db77cb7edaf328bd76cecd5cd5d87e50eca5915d
cnlund <91978441+cnlund@users.noreply.github.com>
2023-12-14 12:58:53 -0500

    14/12 2

--- private/ochurus.go
+++ private/ochurus.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"context"
-	"log"
-	"text/template"
-
-	firebase "firebase.google.com/go"
 	"github.com/gofiber/fiber/v2"
-	"google.golang.org/api/option"
 )
 
 // Zona de los Structs-------------------------------------------------------------------
@@ -32,50 +26,21 @@ type Skills struct {
 // Zona de los Handlers------------------------------------------------------------------
 
 // Handler para enviar el index.html
-func handlerindex(c *fiber.Ctx) error {
-	mensaje := "funciona"
-	template, err := template.ParseFiles("public/index.html")
-	if err != nil {
-		return err
-	}
-	return template.Execute(c, mensaje)
+func indexHandler(c *fiber.Ctx) error {
+	return c.SendFile("public/index.html")
 }
 
-// Handler para enviar hacia postular.html
-func handlerpostular(c *fiber.Ctx) error {
-	mensaje := "el backend te ha movido a postular"
-	template, err := template.ParseFiles("public/postular.html")
-	if err != nil {
-		return err
-	}
-	return template.Execute(c, mensaje)
-}
-
-// Handler para ir a contratar
-func handlercontratar(c *fiber.Ctx) error {
-	template, _ := template.ParseFiles("public/contratar.html")
-	return template.Execute(c, nil)
+// Handler de comprobacion
+func comprohandler(c *fiber.Ctx) error {
+	return c.SendString("El backend esta encendido")
 }
 
 // -----------------------------------------------------------------------------------------
 func main() {
 	// Zona HTML----------------------------------------------------------------------------
 	app := fiber.New()
-	app.Post("/", handlerindex)
-	app.Post("/postular", handlerpostular)
-	//conexion con la api
-	ctx := context.Background()
-	sa := option.WithCredentialsFile("servicekey.json")
-	api, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	client, err := api.Firestore(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer client.Close()
+	app.Get("/", indexHandler)
+	app.Get("/comprobacion", comprohandler)
 	//creamos la conexion del puerto
 	app.Listen(":403")
 }
